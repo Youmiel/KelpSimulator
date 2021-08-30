70d23c0a63fa8e22f666c30bf230e6349034eb54
ishland <ishlandmc@yeah.net>
2021-08-30 20:46:44 +0800

    Add docs

---
+++ README.md
@@ -0,0 +1,28 @@
+# KelpSimulator
+
+## Config
+- `randomTickSpeed`: the same as vanillas `randomTickSpeed` game rule
+- `schedulerFirst`: runs tick scheduler before doing randomTick
+  enable for `<1.15` and `>=1.17` behaviors
+  disable for `1.15-1.16.5` behaviors
+- `waterFlowDelay`: duration in gt between kelp being cut and water source being restored
+- `testLength`: full test duration in gt
+- `harvestPeriod`: interval between harvest operations (allows range definition, see below)
+- `heightLimit`: maximum height in blocks of a full kelp plant (allows range definition, see below)
+
+- `implArgs`: specify which simulation implementation to be used and its extra arguments
+  - `java` specify to use java software simulator
+  - `opencl,<platformid>,<deviceid>` specify to use OpenCL hardware accelerated simulator (default)
+    - `<platformid>` specifies the OpenCL platform id
+    - `<deviceid>` specifies the OpenCL device id under platform
+    - Note: this information will be printed to console when bootstrapping OpenCL hardware acceleration
+- `maxConcurrentTasks`: specify how many simulation tasks can be run at the same time
+  - For Java software simulator, it specifies how many threads can be used by the simulator  
+    Note that values higher than cpu threads count has no effect
+  - For OpenCL hardware accelerated simulator, it specifies how many tasks can be in device queue  
+    Note that higher values can *probably* get higher throughput but higher latency and VRAM usage
+- `oclReductionMode`: (OpenCL hardware accelerated simulator only) specify reduction mode
+  - `NONE`: Full Simulation, produces best result but slower
+  - `REDUCED_KELP_COUNT`: Half simulation, produces approximate results but faster
+  - `REDUCED_TEST_LENGTH` and `REDUCED_BOTH`: DON'T USE: HIGH INACCURACY
+
--- src/main/java/com/ishland/simulations/kelpsimulator/Config.java
+++ src/main/java/com/ishland/simulations/kelpsimulator/Config.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.stream.IntStream;
 
 public class Config {
 
@@ -27,25 +28,23 @@ public class Config {
 
     static {
         final Properties properties = new Properties();
+        boolean created = false;
         try (Reader reader = Files.newBufferedReader(Path.of(".", "config.properties"))) {
             properties.load(reader);
         } catch (IOException e) {
             System.out.println("Error reading config file (" + e + "), creating one...");
+            created = true;
         }
 
         randomTickSpeed = Integer.parseInt(getProperty(properties, "randomTickSpeed", "3"));
         schedulerFirst = Boolean.parseBoolean(getProperty(properties, "schedulerFirst", "true"));
-        waterFlowDelay = Integer.parseInt(getProperty(properties, "waterFlowDelay", "8"));
-        kelpCount = Integer.parseInt(getProperty(properties, "kelpCount", String.valueOf(Short.MAX_VALUE)));
+        waterFlowDelay = Integer.parseInt(getProperty(properties, "waterFlowDelay", "5"));
+        kelpCount = Integer.parseInt(getProperty(properties, "kelpCount", String.valueOf(2048)));
         testLength = Long.parseLong(getProperty(properties, "testLength", String.valueOf(72_000_000)));
-        harvestPeriod = Arrays.stream(getProperty(properties, "harvestPeriod", "600,1200,1800,2400,3000,3600").split(","))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        heightLimit = Arrays.stream(getProperty(properties, "heightLimit", "8,12,16,20,24").split(","))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        harvestPeriod = readRange(getProperty(properties, "harvestPeriod", "r:10,72000,10"));
+        heightLimit = readRange(getProperty(properties, "heightLimit", "r:2,26,1"));
         implArgs = getProperty(properties, "implArgs", "opencl,0,0").split(",");
-        maxConcurrentTasks = Integer.parseInt(getProperty(properties, "maxConcurrentTasks", String.valueOf(Math.min(Runtime.getRuntime().availableProcessors(), 6))));
+        maxConcurrentTasks = Integer.parseInt(getProperty(properties, "maxConcurrentTasks", String.valueOf(Math.min(Runtime.getRuntime().availableProcessors(), 8))));
 
         oclReductionMode = OpenCLSimulationSession.ReductionMode.valueOf(getProperty(properties, "oclReductionMode", OpenCLSimulationSession.ReductionMode.NONE.name()));
 
@@ -53,6 +52,28 @@ public class Config {
             properties.store(writer, "KelpSimulator");
         } catch (IOException e) {
             System.out.println("Unable to save config file: " + e);
+        }
+        if (created) System.exit(0);
+    }
+
+    private static int[] readRange(String property) {
+        if (property.startsWith("r:")) {
+            String selectorString = property.substring("r:".length());
+            final int[] selectors = Arrays.stream(selectorString.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int start = selectors[0];
+            int end = selectors[1];
+            int step = selectors[2];
+            final IntStream.Builder builder = IntStream.builder();
+            for (int i = start; i <= end; i += step) {
+                builder.accept(i);
+            }
+            return builder.build().toArray();
+        } else {
+            return Arrays.stream(property.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
         }
     }
 
